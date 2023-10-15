739a768b4da37b25c32ca2f604d17b41fc0cb285
dantheman <danogbo0@gmail.com>
2023-10-15 17:30:43 +0100

    get req

--- lesson-4/index.js
+++ lesson-4/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
 
+const courses = [
+    { id: 1, title: 'course1'},
+    { id: 2, title: 'course2'},
+    { id: 3, title: 'course3'},
+];
 
 app.get('/', (req, res) => {
     res.send('Hello world')
@@ -10,7 +16,16 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4])
 })
 
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('course wasnt found or doesnt exist');
+    } else {
+        res.send(course);
+    }
+})
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 });
