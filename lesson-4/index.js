aa803313db2ca890e4b3b6b873e139a72c653368
dantheman <danogbo0@gmail.com>
2023-10-15 17:06:03 +0100

    first web server

---
+++ lesson-4/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3, 4])
+})
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+});
