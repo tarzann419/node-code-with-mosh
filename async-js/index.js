551899c7c9b1972b24dbb59f322e079d8b1d7fe3
dantheman <danogbo0@gmail.com>
2023-10-19 12:04:09 +0100

    callback hell

--- async-js/index.js
+++ async-js/index.js
@@ -1,9 +1,20 @@
 console.log('Before')
 
-// callback functions
+// async callback functions
 getUser(1, function(user){
-    console.log('User', user)
+    getRepo( user.username, (repos) => {
+        console.log(repos)
+    } )
+    // CALLBACK HELL
 })
+
+// SYNC CALLBACK
+console.log('Before');
+const user = getUser(1);
+const getRepo = getRepo(repos);
+// const getCommit = getCommits(getRepo[0]);
+console.log('After')
+
 
 console.log('After')
 
@@ -12,4 +23,14 @@ function getUser(id, callback){
         console.log('Reading a user from database...');
         callback({ id:id, username:'tarzann419' });
     }, 2000)
+}
+
+
+function getRepo(username, callback){
+    setTimeout( () => {
+        const repos = ['repo1', 'repo2', 'repo3'];
+        console.log('just a sec...')
+        callback(repos);
+    }, 2000 )
+    return ['repo1', 'repo2', 'repo3']
 }
